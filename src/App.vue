6c2e990f4483f2331e10f772ad6c289753db8601
MarcoPerta <perta.marco@gmail.com>
2022-12-02 14:30:24 +0100

    add fontawesome, main header footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp/>
+
+    <MainComp/>
+
+    <FooterComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import MainComp from './components/MainComp.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp,
+    MainComp,
+    FooterComp
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+/*Import icone regular */
+import {  } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add()
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* import specific icons */
+import {  } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faStar)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
