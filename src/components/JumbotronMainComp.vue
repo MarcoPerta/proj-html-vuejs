c6fd695c0e0276afa1178d5749bc886024037c9d
MarcoPerta <perta.marco@gmail.com>
2022-12-05 14:54:16 +0100

    terzo elemento main

---
+++ src/components/JumbotronMainComp.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="bg-photo">
+        <div class="square">
+                <h4>WE REMOVE THE HEADACHE FROM MOVING</h4>
+                <h2>Move With Ease</h2>
+                <p>With our simple service we can take you from getting a quote,
+                    right through to being in your home with all of your belongings
+                    securely packed and safely delivered, even in the rooms of your choice.
+                </p>
+                <button class="btn-blue">GET A FREE QUOTE NOW</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'JumbotronMainComp',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+.bg-photo {
+    height: 500px;
+    background-image: url(../assets/images/avada-movers-movewithease-background-new.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 45px;
+    .square {
+            width: 455px;
+            background-color: #6bbb14;
+            border: 10px solid #4c8d53;
+            color: white;
+            align-self: center;
+            padding: 30px;
+
+            h4,h2,p{
+                padding: 10px 0px;
+            }
+
+            h2{
+                border-bottom: 1px solid white;
+                
+            }
+
+            p{
+                line-height: 25px;
+            }
+    
+            .btn-blue {
+                color: white;
+                border: none;
+                padding: 15px;
+                background-color: #1975c0;
+                border-radius: 30px;
+                margin-left: 15px;
+                margin-bottom: 15px;
+            }
+        }
+    }
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -4,18 +4,22 @@
 
         <!-- componente con all'interno le card ciclate -->
         <ServiceComp :ServicesCards="ServicesCards"/>
+
+        <JumbotronMainComp/>
     </div>
 </template>
 
 <script>
 import HeadMainComp from './HeadMainComp.vue'
 import ServiceComp from './ServiceComp.vue'
+import JumbotronMainComp from './JumbotronMainComp.vue'
 
 export default {
     name: 'MainComp',
     components: {
         HeadMainComp,
-        ServiceComp
+        ServiceComp,
+        JumbotronMainComp
     },
     props: {
         ServicesCards: Array
